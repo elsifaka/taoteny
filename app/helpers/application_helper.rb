6df10dfd53aed039c139706a36af66e2a05dc887
tosoa <tosoa@lunatex.local>
2008-06-16 00:36:37 +0300

    added gravatar helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
+require 'digest/md5'
 module ApplicationHelper
   def language_select(user, usage)
     render :partial => "languages/collection_select", :locals => { :object => user, :usage => usage }
+  end
+
+  def gravatar_url_for(email, options = {})    
+    url_for({ :gravatar_id => Digest::MD5.hexdigest(email), :host => 'www.gravatar.com',
+        :protocol => 'http://', :only_path => false, :controller => 'avatar.php'    
+      }.merge(options))  
   end
 end
