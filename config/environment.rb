f67b3adddffe05c9cd1b7787796fba9c66a5bdea
tosoa <tosoa@lunatex.local>
2008-06-16 16:51:42 +0300

    updated lang files

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # used by globalite.
-ENV['BASE_LOCALE'] = 'mg-MG'
+ENV['BASE_LOCALE'] = 'mg'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.1' unless defined? RAILS_GEM_VERSION
